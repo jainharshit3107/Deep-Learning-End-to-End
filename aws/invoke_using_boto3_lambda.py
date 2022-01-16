408e1041c79c4375742b99fa74b00c07e5a18c2d
Harshit Jain <jainharshit3107@gmail.com>
2022-01-16 18:01:47 +0530

    Update invoke_using_boto3_lambda.py

--- aws/invoke_using_boto3_lambda.py
+++ aws/invoke_using_boto3_lambda.py
@@ -1,1 +1,9 @@
+import boto3
+import math
+import dateutil
+import json
+import os
 
+# grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+client = boto3.client(service_name='sagemaker-runtime')
